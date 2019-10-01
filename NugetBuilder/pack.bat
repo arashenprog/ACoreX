372191252d3133bfc115320d3ae6bd091e4d05ae
Masoud Tahmasebi <mtahmasebi118@gmail.com>
2019-10-01 17:04:18 +0330

    pack modificatiions

--- NugetBuilder/pack.bat
+++ NugetBuilder/pack.bat
@@ -1,2 +1,16 @@
 nuget pack ..\AssemblyLoader\ACoreX.AssemblyLoader\ACoreX.AssemblyLoader.csproj
+nuget pack ..\Authentication\ACoreX.Authentication.Abstractions\ACoreX.Authentication.Abstractions.csproj
+nuget pack ..\Authentication\ACoreX.Authentication.Core\ACoreX.Authentication.Core.csproj
+nuget pack ..\Authentication\ACoreX.Authentication.JWT\ACoreX.Authentication.JWT.csproj
+nuget pack ..\Configurations\ACoreX.Configurations.Abstractions\ACoreX.Configurations.Abstractions.csproj
+nuget pack ..\Data\ACoreX.Data.Abstractions\ACoreX.Data.Abstractions.csproj
+nuget pack ..\Data\ACoreX.Data.Core\ACoreX.Data.Core.csproj
+nuget pack ..\Data\ACoreX.Data.Dapper\ACoreX.Data.Dapper.csproj
+nuget pack ..\Data\ACoreX.Data.EFCore\ACoreX.Data.EFCore.csproj
+nuget pack ..\Extensions\ACoreX.Base.BaseExtensions\ACoreX.Extensions.Base.Abstractions.csproj
+nuget pack ..\Extensions\ACoreX.Extensions.Types\ACoreX.Extensions.Types.csproj
+nuget pack ..\Extensions\ACoreX.Plugin\ACoreX.Plugin.csproj
+nuget pack ..\Injector\ACoreX.Injector.Abstraction\ACoreX.Injector.Abstractions.csproj
+nuget pack ..\Injector\ACoreX.Injector.Core\ACoreX.Injector.Core.csproj
+nuget pack ..\WebAPI\ACoreX.WebAPI\ACoreX.WebAPI.csproj
 xcopy /y *.nupkg  \\192.168.105.55\LocalNuget

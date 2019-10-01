83267cad86e99cc1446b39469b4425760604796a
arashenprog <arash.oshnoudi@gmail.com>
2019-10-01 16:41:00 +0330

    fix namespace

--- Configurations/ACoreX.Configurations.Abstractions/ISettingManager.cs
+++ Configurations/ACoreX.Configurations.Abstractions/ISettingManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ACoreX.Configurations
+namespace ACoreX.Configurations.Abstractions
 {
     public interface ISettingManager
     {

53cc05568bce04eda1bd4fd2d444847d56a5f10b
Masoud Tahmasebi <mtahmasebi118@gmail.com>
2019-12-01 16:08:36 +0330

    delete nuget and update pack

--- NugetBuilder/pack.bat
+++ NugetBuilder/pack.bat
@@ -1,22 +1,22 @@
-nuget pack ..\AssemblyLoader\ACoreX.AssemblyLoader\ACoreX.AssemblyLoader.csproj
-nuget pack ..\Authentication\ACoreX.Authentication.Abstractions\ACoreX.Authentication.Abstractions.csproj
-nuget pack ..\Authentication\ACoreX.Authentication.Core\ACoreX.Authentication.Core.csproj
-nuget pack ..\Authentication\ACoreX.Authentication.JWT\ACoreX.Authentication.JWT.csproj
+dotnet pack ..\AssemblyLoader\ACoreX.AssemblyLoader\ACoreX.AssemblyLoader.csproj -o ./
+dotnet pack ..\Authentication\ACoreX.Authentication.Abstractions\ACoreX.Authentication.Abstractions.csproj -o ./
+dotnet pack ..\Authentication\ACoreX.Authentication.Core\ACoreX.Authentication.Core.csproj -o ./
+dotnet pack ..\Authentication\ACoreX.Authentication.JWT\ACoreX.Authentication.JWT.csproj -o ./
 
-nuget pack ..\Configurations\ACoreX.Configurations.Abstractions\ACoreX.Configurations.Abstractions.csproj
-nuget pack ..\Configurations\ACoreX.Configurations.NetCore\ACoreX.Configurations.NetCore.csproj
+dotnet pack ..\Configurations\ACoreX.Configurations.Abstractions\ACoreX.Configurations.Abstractions.csproj -o ./
+dotnet pack ..\Configurations\ACoreX.Configurations.NetCore\ACoreX.Configurations.NetCore.csproj -o ./
 
-nuget pack ..\Data\ACoreX.Data.Abstractions\ACoreX.Data.Abstractions.csproj
-nuget pack ..\Data\ACoreX.Data.Dapper\ACoreX.Data.Dapper.csproj
-nuget pack ..\Data\ACoreX.Data.EFCore\ACoreX.Data.EFCore.csproj
-nuget pack ..\Extensions\ACoreX.Base.BaseExtensions\ACoreX.Extensions.Base.Abstractions.csproj
-nuget pack ..\Extensions\ACoreX.Extensions.Types\ACoreX.Extensions.Types.csproj
-nuget pack ..\Extensions\ACoreX.Plugin\ACoreX.Plugin.csproj
-nuget pack ..\Injector\ACoreX.Injector.Abstraction\ACoreX.Injector.Abstractions.csproj
-nuget pack ..\Injector\ACoreX.Injector.Core\ACoreX.Injector.Core.csproj
-nuget pack ..\Injector\ACoreX.Injector.NetCore\ACoreX.Injector.NetCore.csproj
+dotnet pack ..\Data\ACoreX.Data.Abstractions\ACoreX.Data.Abstractions.csproj -o ./
+dotnet pack ..\Data\ACoreX.Data.Dapper\ACoreX.Data.Dapper.csproj -o ./
+dotnet pack ..\Data\ACoreX.Data.EFCore\ACoreX.Data.EFCore.csproj -o ./
+dotnet pack ..\Extensions\ACoreX.Base.BaseExtensions\ACoreX.Extensions.Base.Abstractions.csproj -o ./
+dotnet pack ..\Extensions\ACoreX.Extensions.Types\ACoreX.Extensions.Types.csproj -o ./
+dotnet pack ..\Extensions\ACoreX.Plugin\ACoreX.Plugin.csproj -o ./
+dotnet pack ..\Injector\ACoreX.Injector.Abstraction\ACoreX.Injector.Abstractions.csproj -o ./
+dotnet pack ..\Injector\ACoreX.Injector.Core\ACoreX.Injector.Core.csproj -o ./
+dotnet pack ..\Injector\ACoreX.Injector.NetCore\ACoreX.Injector.NetCore.csproj -o ./
 
-nuget pack ..\WebAPI\ACoreX.WebAPI\ACoreX.WebAPI.csproj
-nuget pack ..\WebAPI\ACoreX.WebAPI.Abstractions\ACoreX.WebAPI.Abstractions.csproj
+dotnet pack ..\WebAPI\ACoreX.WebAPI\ACoreX.WebAPI.csproj -o ./
+dotnet pack ..\WebAPI\ACoreX.WebAPI.Abstractions\ACoreX.WebAPI.Abstractions.csproj -o ./
 xcopy /y *.nupkg  \\192.168.105.55\LocalNuget
 efg generate
